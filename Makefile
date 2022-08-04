c43fee40ad1851affe72df30ac565fd4da0eccbf
EC2 Default User <ec2-user@ip-172-31-30-9.ec2.internal>
2022-08-04 15:53:15 +0000

    restarting to build the image

--- Makefile
+++ Makefile
@@ -8,8 +8,8 @@
 setup:
 	# Create python virtualenv & source it
 	# source ~/.devops/bin/activate
-	python3 -m venv ./devops
-	./devops/bin/activate
+	python3 -m venv ~/.devops
+	source ~/.devops/bin/activate
 
 install:
 	# This should be run from inside a virtualenv
--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t ml-microservice-kubernetes .
+docker build -t ml-kubernetes .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 ml-microservice-kubernetes
+docker run -p 8000:80 ml-kubernetes
